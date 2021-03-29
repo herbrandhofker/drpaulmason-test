baed93af2be464929cb47f7ad1a2f05114fffdec
Herbrand <Herbrand.Hofker@gmail.com>
2021-03-29 23:06:28 +0200

    aa

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js'
     },
